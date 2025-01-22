4504c89419b7f11e9a4187906adc96f095f7c154
MichaelBeaudet <michaeltbeaudetalt@gmail.com>
2025-01-22 10:03:43 -0600

    Update program_2.py
    I think I turned this in right

--- program_2.py
+++ program_2.py
@@ -1,6 +1,6 @@
 def proverbs_22_29():
     # Write your Proverbs 22:29 code below
-    print('...')
+    print('Seest thou a man diligent in his business? he shall stand before kings; he shall not stand before mean men.')
     
 
 proverbs_22_29()
